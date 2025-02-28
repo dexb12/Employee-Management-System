57f1894a7a6e42d20354e01dc2e64a02aef9c606
Dexter <balindresdexter20@gmail.com>
2025-03-01 00:00:42 +0800

    MySQL DB Connection sample

---
+++ config/dbConnection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+require("dotenv").config({ path: "../.env" });
+
+console.log("Database Name: ", process.env.DATABASE_NAME);
+console.log("DB_Password: ", process.env.MYSQL_PASSWORD);
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: process.env.DATABASE_NAME,
+  password: process.env.MYSQL_PASSWORD,
+});
+
+pool.getConnection((err) => {
+  if (err) throw err;
+  console.log("MySQL Database Connected!");
+});
